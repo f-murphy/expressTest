b7e51cce02f0d5daeaeb0e2bb76a1ac82fe7755a
f-murphy <murphy.fariz@gmail.com>
2022-11-02 18:53:01 +0300

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const PORT = 5000;
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.status(200).json('Сервер работает')
+})
+   
+
+app.listen(PORT, () => console.log('Server started on port ' + PORT))
