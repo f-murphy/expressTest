20be206485d30905c1f3df4efe88d68332acc8e2
f-murphy <100728686+f-murphy@users.noreply.github.com>
2022-11-06 20:36:44 +0300

    added Post.Model

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 import express from 'express'
 import mongoose from 'mongoose';
+import Post from './Post.js';
 
 const PORT = 5000;
 const DB_URL = 'mongodb+srv://testExpress:EWQ5doMQs2nhjIup@cluster0.4olxvpw.mongodb.net/?retryWrites=true&w=majority'
 
-const run = express()
+const app = express()
 
-run.use(express.json())
+app.use(express.json())
 
-run.post('/', (req, res) => {
-    console.log(req.body)
+app.post('/', (req, res) => {
+    const post = Post.create()
     res.status(200).json('server works')
 })
-   
 
-run.listen(PORT, () => console.log('Server started on port ' + PORT))
+async function startApp(){
+    try{
+        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+        app.listen(PORT, () => console.log('Server started on port ' + PORT))
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+startApp()
