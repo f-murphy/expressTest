c805eb309998dbe19ae59e59d12c429f7246b329
f-murphy <100728686+f-murphy@users.noreply.github.com>
2022-11-06 19:57:59 +0300

    add DB_URL, mongoose

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
 
 const PORT = 5000;
+const DB_URL = 'mongodb+srv://testExpress:EWQ5doMQs2nhjIup@cluster0.4olxvpw.mongodb.net/?retryWrites=true&w=majority'
 
-const appRun = express()
+const run = express()
 
-appRun.use(express.json())
+run.use(express.json())
 
-appRun.post('/', (req, res) => {
+run.post('/', (req, res) => {
     console.log(req.body)
     res.status(200).json('server works')
 })
    
 
-appRun.listen(PORT, () => console.log('Server started on port ' + PORT))
+run.listen(PORT, () => console.log('Server started on port ' + PORT))
